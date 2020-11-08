488efae8eaa2ff5c8e6f66d8e81b3eb9b513d155
unknown <jrussellchan2000@gmail.com>
2020-11-09 01:03:36 +0700

    new productQuantity polymorph :sparkles:

---
+++ database/migrations/2020_11_08_092329_create_product_quantities_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductQuantitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_quantities', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->string('product_quantitable_id');
+            $table->string('product_quantitable_type');
+            $table->integer('quantity');
+            $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_quantities');
+    }
+}

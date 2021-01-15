3faacea5ba280419f910d29fceba93bf54d165fa
unknown <jrussellchan2000@gmail.com>
2021-01-15 18:10:08 +0700

    debug notif server 2

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -16,7 +16,7 @@ class PaymentController extends Controller
         $notification = json_decode($payload);
 
         $validSignatureKey = hash("sha512", $notification->order_id . $notification->status_code . $notification->gross_amount . env('MIDTRANS_SERVER_KEY'));
-        return $validSignatureKey;
+        // return $validSignatureKey;
 
         if ($notification->signature_key != $validSignatureKey) {
             return response(['message' => 'Invalid signature'], 403);

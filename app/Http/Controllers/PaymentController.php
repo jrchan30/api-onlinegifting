a9879aa80033aa4b52e952f24f00c293f8dad39f
unknown <jrussellchan2000@gmail.com>
2021-01-15 18:11:02 +0700

    debug notif server 3

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -19,7 +19,7 @@ class PaymentController extends Controller
         // return $validSignatureKey;
 
         if ($notification->signature_key != $validSignatureKey) {
-            return response(['message' => 'Invalid signature'], 403);
+            return response(['message' => 'Invalid signature'], 402);
         }
 
         // $this->initPaymentGateway();
